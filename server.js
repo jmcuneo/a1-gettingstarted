64b42d96ce2e286bc7b03c5ef51bffab1d43ecc0
Sargeanthost <sargeanthost@gmail.com>
2024-03-13 05:44:59 -0400

    server changes

--- server.js
+++ server.js
@@ -1,24 +1,36 @@
 const http = require('http'),
-      fs   = require('fs'),
-      port = 3000
+  fs = require('fs'),
+  port = 3000
 
-const server = http.createServer( function( request,response ) {
-  switch( request.url ) {
+const server = http.createServer(function (request, response) {
+  switch (request.url) {
     case '/':
-      sendFile( response, 'index.html' )
+      sendFile(response, 'index.html')
       break
     case '/index.html':
-      sendFile( response, 'index.html' )
+      sendFile(response, 'index.html')
+      break
+    case '/script.js':
+      sendFile(response, 'script.js')
+      break
+    case '/style.css':
+      sendFile(response, 'style.css')
+      break
+    case '/imgs/atlas.jpg':
+      sendFile(response, 'imgs/atlas.jpg')
+      break
+    case '/imgs/northrop.jpg':
+      sendFile(response, 'imgs/northrop.jpg')
       break
     default:
-      response.end( '404 Error: File Not Found' )
+      response.end('404 Error: File Not Found')
   }
 })
 
-server.listen( process.env.PORT || port )
+server.listen(process.env.PORT || port)
 
-const sendFile = function( response, filename ) {
-   fs.readFile( filename, function( err, content ) {
-     response.end( content, 'utf-8' )
-   })
+const sendFile = function (response, filename) {
+  fs.readFile(filename, function (err, content) {
+    response.end(content, 'utf-8')
+  })
 }
