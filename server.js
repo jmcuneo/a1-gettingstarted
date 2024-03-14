02597211ac35b4bd16b3b567f27b70cf56e7dd7c
AGChad <agchadbourne@wpi.edu>
2024-03-14 12:51:11 -0400

    uhhhh i accidentally finished the homework

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ const server = http.createServer( function( request,response ) {
     case '/index.html':
       sendFile( response, 'index.html' )
       break
-    case '/test.css':
-      sendFile( response, 'test.css' )
+    case '/style.css':
+      sendFile( response, 'style.css' )
       break
     case '/test.js':
       sendFile( response, 'test.js' )
--- test.js
+++ test.js
@@ -1,1 +1,25 @@
-alert("Hellow World");
+window.onload = function() {
+    var modal = document.getElementById("modal");
+
+    var img = document.getElementById("sugar");
+    var modalImg = document.getElementById("bigSugar");
+    img.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+    }
+    var span = document.getElementsByClassName("close")[0];
+    span.onclick = function() { 
+        modal.style.display = "none";
+    }
+
+    var footer = document.getElementById("footer");
+    var details = document.getElementById("details");
+    details.addEventListener("toggle", (event) => {
+        if (details.open) {
+            footer.style.paddingTop = "0%";
+        } else {
+            footer.style.paddingTop = "3%";
+        }
+    });
+}
+
