1cb4e1a264d9840f18bc8a6c60aa47e5fe71c138
Nora Cleary <120121240+ngcleary@users.noreply.github.com>
2024-03-16 16:25:43 -0400

    Stopped move function when reaches mid screen

--- animation.js
+++ animation.js
@@ -1,6 +1,9 @@
-const left = 0; //distance between left side of the screen and left of plane image
+var left = 0; //distance between left side of the screen and left of plane image
 const flyPlane = function(){
-  left = left + 20;
+  left = left + 10;
+  if(left >= 530){
+    return;
+  }
   document.getElementById('plane').style.left = left + 'px';
-  setTimeout(flyPlane, 100);
+  setTimeout(flyPlane, 50);
 }
