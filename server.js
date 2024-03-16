b5e34ffac7e689ec4e76988f660e27577788684a
DocterBuster <87544221+DocterBuster@users.noreply.github.com>
2024-03-16 10:51:00 -0400

    Test changes to push to Glitch

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const server = http.createServer( function( request,response ) {
     case '/index.html':
       sendFile( response, 'index.html' )
       break
+    case '/pinguim-penguin.gif':
+      sendFile( response, '/pinguim-penguin.gif' )
+      break
     default:
       response.end( '404 Error: File Not Found' )
   }
