a5bc2f49ff352fc79c8535096984349aa1f7e316
Azu109 <93104542+Azu109@users.noreply.github.com>
2024-03-17 19:50:11 -0400

    update

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const http = require('http'),
       fs   = require('fs'),
       port = 3000
 
+
+
 const server = http.createServer( function( request,response ) {
   switch( request.url ) {
     case '/':
@@ -9,6 +11,16 @@ const server = http.createServer( function( request,response ) {
       break
     case '/index.html':
       sendFile( response, 'index.html' )
+      break
+    case '/resource/image.png':
+      var img = fs.readFileSync('./resource/image.png');
+      res.writeHead(200, {'Content-Type': 'image/png' });
+      res.end(img, 'binary');
+      break
+    case '/resource/theme.jpeg':
+      var img = fs.readFileSync('./resource/theme.jpeg');
+      res.writeHead(200, {'Content-Type': 'image/jpg' });
+      res.end(img, 'binary');
       break
     default:
       response.end( '404 Error: File Not Found' )
