a56ab3674a1caa08d21433f035d6dc2a9332eb8f
Austin Rebello <55983501+AustinRebello@users.noreply.github.com>
2024-03-17 16:21:11 -0400

    Minor edits and comments

--- scripts.js
+++ scripts.js
@@ -1,12 +1,16 @@
-
-
-
+/**
+* Function that moves the box on the screen
+*/
 function moveBox(){
     var theBox = document.getElementById("box");
     console.log(theBox.width);
     theBox.style.width = 2 + "%";
     var width = 2;
     var timer = setInterval(runWidth, 1);
+
+    /**
+     * Function that extends the width of the box
+     */
     function runWidth(){
         width = width + 2
         if( width == 52){
@@ -16,4 +20,5 @@ function moveBox(){
         theBox.style.width = width + "%";
         }
     }
+    
 }
