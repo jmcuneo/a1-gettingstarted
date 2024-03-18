ade0a3a900809456669c367f84875f20b59d73ee
aniginger <cspgithub@twinleaf.xyz>
2024-03-17 22:40:10 -0400

    Modified server.js to allow for stylization

--- server.js
+++ server.js
@@ -1,24 +1,40 @@
+// Modified version of the code used to support stylization
+
 const http = require('http'),
       fs   = require('fs'),
-      port = 3000
+      port = 3000;
 
-const server = http.createServer( function( request,response ) {
-  switch( request.url ) {
-    case '/':
-      sendFile( response, 'index.html' )
-      break
-    case '/index.html':
-      sendFile( response, 'index.html' )
-      break
-    default:
-      response.end( '404 Error: File Not Found' )
+const server = http.createServer(function(request, response) {
+  if (request.url === '/' || request.url === '/index.html') {
+    sendFile(response, 'index.html');
+  } else {
+    // Attempt to serve static files
+    sendFile(response, request.url.substring(1)); // Remove the leading '/'
   }
-})
+});
 
-server.listen( process.env.PORT || port )
+server.listen(process.env.PORT || port);
 
-const sendFile = function( response, filename ) {
-   fs.readFile( filename, function( err, content ) {
-     response.end( content, 'utf-8' )
-   })
-}
+const sendFile = function(response, filename) {
+   fs.readFile(filename, function(err, content) {
+     if (err) {
+       response.writeHead(404, { 'Content-Type': 'text/plain' });
+       response.end('404 Error: File Not Found');
+     } else {
+       // Determine the content type based on the file extension
+       let contentType = 'text/html'; // Default content type
+       if (filename.endsWith('.css')) {
+         contentType = 'text/css';
+       } else if (filename.endsWith('.js')) {
+         contentType = 'text/javascript';
+       } else if (filename.endsWith('.png')) {
+         contentType = 'image/png';
+       } else if (filename.endsWith('.jpg') || filename.endsWith('.jpeg')) {
+         contentType = 'image/jpeg';
+       }
+       
+       response.writeHead(200, { 'Content-Type': contentType });
+       response.end(content, 'utf-8');
+     }
+   });
+};
