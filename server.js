f309b12e6156ce62d29693434fc47551b632397f
whuang214 <whuang2146@gmail.com>
2024-03-17 20:43:41 -0400

    switched over to express

--- server.js
+++ server.js
@@ -1,24 +1,18 @@
-const http = require('http'),
-      fs   = require('fs'),
-      port = 3000
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
 
-const server = http.createServer( function( request,response ) {
-  switch( request.url ) {
-    case '/':
-      sendFile( response, 'index.html' )
-      break
-    case '/index.html':
-      sendFile( response, 'index.html' )
-      break
-    default:
-      response.end( '404 Error: File Not Found' )
-  }
-})
+app.use(express.static("."));
 
-server.listen( process.env.PORT || port )
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
-const sendFile = function( response, filename ) {
-   fs.readFile( filename, function( err, content ) {
-     response.end( content, 'utf-8' )
-   })
-}
+app.use((req, res, next) => {
+  res.status(404).send("404 Error: File Not Found");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
