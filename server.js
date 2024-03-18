9cc8c0b5b9abcc6a8836d500106b23026b5deb91
RyanNguyen321 <nguyenryan0903@gmail.com>
2024-03-17 21:50:52 -0400

    Fixed animation and css to work with glitch

--- server.js
+++ server.js
@@ -7,18 +7,8 @@ const server = http.createServer( function( request,response ) {
     case '/':
       sendFile( response, 'index.html' )
       break
-    // case '/index.html':
-    //   sendFile( response, 'index.html' )
-    //   break
-    // case '/style.css':
-    //   sendFile( response, 'style.css' )
-    //   break
-    // case '/animation.js':
-    //   sendFile( response, 'animation.js' )
-    //   break
     default:
-      sendFile(response, response.url)
-      // response.end( '404 Error: File Not Found' )
+      response.end( '404 Error: File Not Found' )
   }
 })
 
